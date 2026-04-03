330fc8c4b92b161687e5f687dbc9090d093724ca
Kenneth Fadojutimi <13kken@Kenneths-MacBook-Air.local>
2026-04-02 20:39:32 -0400

    add games.url

--- backend/GameHaven/urls.py
+++ backend/GameHaven/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),  # users endpoints
-   # path('api/', include('games.urls')),  # games endpoints
+    path('api/', include('games.urls')),  # games endpoints
 ]
