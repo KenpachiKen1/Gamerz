4a847f6e30d1079f8bab53817fa371d38375040f
Bryson Moore <bmoore70020@gmail.com>
2026-04-16 19:25:08 -0400

    Add CRUD tests for Game model

--- backend/games/tests.py
+++ backend/games/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from games.models import Game         # add this
 
 # Create your tests here.
+
+class TestGameCRUD(TestCase):
+    def test_create_game(self):
+        game = Game.objects.create(title="nba2k")
+        self.assertEqual(Game.objects.count(), 1)
+        self.assertEqual(game.title, "nba2k")
+
+    def test_delete_game(self):             
+        game = Game.objects.create(title="Elden Ring")
+        game.delete()
+        self.assertEqual(Game.objects.count(), 0) 
+    
+    def test_update_game(self):
+        game = Game.objects.create(title="nba2k")
+        game.title = "2k"
+        game.save()
+        updated = Game.objects.get(pk=game.pk)
+        self.assertEqual(updated.title, "NBA 2K25")
+    
+    def test_read_game(self):
+        Game.objects.create(title="nba2k")
+        game = Game.objects.get(title="nba2k")
+        self.assertEqual(game.title, "nba2k")
