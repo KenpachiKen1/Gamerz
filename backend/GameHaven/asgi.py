bd0373a670a64e47055199a3af549f415cbd677e
Kenneth Fadojutimi <13kken@Kenneths-MacBook-Air.local>
2026-05-02 14:20:28 -0400

    fixed asgi bug hopefully.

--- backend/GameHaven/asgi.py
+++ backend/GameHaven/asgi.py
@@ -1,18 +1,21 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GameHaven.settings")
+
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 from chatrooms.routing import websocket_urlpatterns
 from chatrooms.middleware import FirebaseAuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GameHaven.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-
+    "http": django_asgi_app,
     "websocket": FirebaseAuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
